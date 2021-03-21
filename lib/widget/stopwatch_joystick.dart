5be700dedd8284c8baec70deb1aadd22dbcc0d8b
Tomi <seaud5@gmail.com>
2021-03-21 12:03:16 +0900

    Place the left and right buttons.

--- lib/widget/stopwatch_joystick.dart
+++ lib/widget/stopwatch_joystick.dart
@@ -7,22 +7,69 @@ import 'package:stopwatch/bloc/stopwatch_bloc.dart';
 class StopwatchJoystick extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bloc = BlocProvider.of<StopwatchBloc>(context);
     return Padding(
         padding: const EdgeInsets.only(bottom: 30.0),
-        child: _centerButton(BlocProvider.of<StopwatchBloc>(context)));
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _leftButton(bloc),
+            _centerButton(bloc),
+            _rightButton(bloc),
+          ],
+        ));
   }
 
-  Widget _centerButton(StopwatchBloc bloc) {
+  Widget _leftButton(StopwatchBloc bloc) {
     return SizedBox(
       width: 60.0,
       height: 60.0,
       child: BlocBuilder<StopwatchBloc, StopwatchState>(
         builder: (context, state) {
-          if (state is StopwatchPlaying) {
-            return _pauseButton(bloc);
-          } else {
-            return _playButton(bloc);
-          }
+          return IconButton(
+              padding: const EdgeInsets.all(0.0),
+              icon: Icon(
+                Icons.cancel,
+                size: 60.0,
+              ),
+              onPressed: () {});
+        },
+      ),
+    );
+  }
+
+  Widget _centerButton(StopwatchBloc bloc) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 15, right: 15),
+      child: SizedBox(
+        width: 60.0,
+        height: 60.0,
+        child: BlocBuilder<StopwatchBloc, StopwatchState>(
+          builder: (context, state) {
+            if (state is StopwatchPlaying) {
+              return _pauseButton(bloc);
+            } else {
+              return _playButton(bloc);
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _rightButton(StopwatchBloc bloc) {
+    return SizedBox(
+      width: 60.0,
+      height: 60.0,
+      child: BlocBuilder<StopwatchBloc, StopwatchState>(
+        builder: (context, state) {
+          return IconButton(
+              padding: const EdgeInsets.all(0.0),
+              icon: Icon(
+                Icons.add_circle,
+                size: 60.0,
+              ),
+              onPressed: () {});
         },
       ),
     );
