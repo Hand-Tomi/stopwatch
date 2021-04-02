8342ec97696f97d7ec42f5f0118170032a69a3a9
Tomi <seaud5@gmail.com>
2021-04-03 00:22:40 +0900

    Change Lap's constructor to const

--- lib/model/lap.dart
+++ lib/model/lap.dart
@@ -5,7 +5,7 @@ class Lap extends Equatable {
   final int lapTime;
   final int splitTime;
 
-  Lap(this.lap, this.lapTime, this.splitTime);
+  const Lap(this.lap, this.lapTime, this.splitTime);
 
   @override
   List<Object> get props => throw UnimplementedError();
