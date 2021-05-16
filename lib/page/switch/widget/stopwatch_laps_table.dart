ddf3318b3371b3d293f311549fd5ba41d4e36026
Tomi <seaud5@gmail.com>
2021-05-16 17:48:47 +0900

    Lap테이블의 Column을 함수화한다.

--- lib/page/switch/widget/stopwatch_laps_table.dart
+++ lib/page/switch/widget/stopwatch_laps_table.dart
@@ -10,36 +10,29 @@ class StopwatchLapsTable extends StatelessWidget {
     return Expanded(
       child: BlocBuilder<LapsBloc, List<Lap>>(
         builder: (context, state) {
-          final columnTextStyle = Theme.of(context)
-              .textTheme
-              .subtitle2
-              ?.copyWith(fontStyle: FontStyle.italic);
           return SingleChildScrollView(
             child: DataTable(
               columns: <DataColumn>[
-                DataColumn(
-                  label: Text(
-                    'Lap',
-                    style: columnTextStyle,
-                  ),
-                ),
-                DataColumn(
-                  label: Text(
-                    'Lap Times',
-                    style: columnTextStyle,
-                  ),
-                ),
-                DataColumn(
-                  label: Text(
-                    'Split Time',
-                    style: columnTextStyle,
-                  ),
-                ),
+                createDataColumn(context, 'Lap'),
+                createDataColumn(context, 'Lap Times'),
+                createDataColumn(context, 'Split Time'),
               ],
               rows: state.reversed.map((lap) => createDataRow(lap)).toList(),
             ),
           );
         },
+      ),
+    );
+  }
+
+  DataColumn createDataColumn(BuildContext context, String title) {
+    return DataColumn(
+      label: Text(
+        title,
+        style: Theme.of(context)
+            .textTheme
+            .subtitle2
+            ?.copyWith(fontStyle: FontStyle.italic),
       ),
     );
   }
