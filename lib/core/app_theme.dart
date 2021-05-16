56dd10300a5a6d2d19fcd7e5ad36e76039e9cf73
Tomi <seaud5@gmail.com>
2021-05-16 16:48:02 +0900

    기본 Theme를 light로 지정해두다.
    나중에, dark모드 대응할 때 간단히 할 수 있도록

--- lib/core/app_theme.dart
+++ lib/core/app_theme.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static ThemeData get theme {
-    return ThemeData(
+    final originalTheme = ThemeData.light();
+    return originalTheme.copyWith(
       primaryColor: const Color(0xFFA64962),
       accentColor: const Color(0xFF1D2526),
       dividerColor: const Color(0xffCCCCCC),
