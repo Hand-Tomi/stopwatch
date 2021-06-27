44b1d2113a1bab84f503c6f0d10f419cc3c73ac5
Tomi <seaud5@gmail.com>
2021-06-27 16:13:37 +0900

    History데이터 클래스를 Equatable화 하였다.
    EquatableMixin버그가 해결됨.

--- lib/model/history.dart
+++ lib/model/history.dart
@@ -1,3 +1,4 @@
+import 'package:equatable/equatable.dart';
 import 'package:hive/hive.dart';
 
 import 'lap.dart';
@@ -5,7 +6,7 @@ import 'lap.dart';
 part 'history.g.dart';
 
 @HiveType(typeId: 1)
-class History extends HiveObject {
+class History extends HiveObject with EquatableMixin {
   @HiveField(0)
   final int msec;
 
@@ -17,16 +18,12 @@ class History extends HiveObject {
 
   History(this.msec, this.savedAt, {this.laps});
 
-  // 버그가 있어 `EquatableMixin`을 사용하면 테스트에서 에러가 발생하고 있어,
-  // 임시로 이 함수를 이용해 비교하도록 한다.
-  // https://github.com/felangel/equatable/issues/115
-  bool isSame(History history) {
-    return this.msec == history.msec && this.savedAt == history.savedAt;
-  }
-
   History copyWith({int? msec, DateTime? savedAt, List<Lap>? laps}) => History(
         msec ?? this.msec,
         savedAt ?? this.savedAt,
         laps: laps ?? this.laps,
       );
+
+  @override
+  List<Object?> get props => [msec, savedAt, laps];
 }
